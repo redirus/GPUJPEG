f5cfeaf68ddab1f370228a6c788b44c779b38d36
redirus <yiyxio@pku.edu.cn>
2018-04-11 23:51:22 +0800

    first commit

---
+++ build/CMakeFiles/gpujpeg.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gpujpeg.dir/src/./gpujpeg_generated_gpujpeg_huffman_gpu_encoder.cu.o"
+  "CMakeFiles/gpujpeg.dir/src/./gpujpeg_generated_gpujpeg_preprocessor.cu.o"
+  "CMakeFiles/gpujpeg.dir/src/./gpujpeg_generated_gpujpeg_dct_gpu.cu.o"
+  "CMakeFiles/gpujpeg.dir/src/./gpujpeg_generated_gpujpeg_huffman_gpu_decoder.cu.o"
+  "CMakeFiles/gpujpeg.dir/src/gpujpeg_writer.cpp.o"
+  "CMakeFiles/gpujpeg.dir/src/gpujpeg_huffman_cpu_encoder.cpp.o"
+  "CMakeFiles/gpujpeg.dir/src/gpujpeg_huffman_cpu_decoder.cpp.o"
+  "CMakeFiles/gpujpeg.dir/src/gpujpeg_table.cpp.o"
+  "CMakeFiles/gpujpeg.dir/src/gpujpeg_reader.cpp.o"
+  "CMakeFiles/gpujpeg.dir/src/gpujpeg_common.cpp.o"
+  "CMakeFiles/gpujpeg.dir/src/gpujpeg_decoder.cpp.o"
+  "CMakeFiles/gpujpeg.dir/src/gpujpeg_dct_cpu.cpp.o"
+  "CMakeFiles/gpujpeg.dir/src/gpujpeg_encoder.cpp.o"
+  "libgpujpeg.pdb"
+  "libgpujpeg.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gpujpeg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
